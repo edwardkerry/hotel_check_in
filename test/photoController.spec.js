a688df2794b25f9c65ee94cd5ec4f2eeb618f88d
Chris Wynne <chris.wynndow@gmail.com>
2016-02-10 16:37:42 +0000

    Updates indentations

--- test/photoController.spec.js
+++ test/photoController.spec.js
@@ -1,38 +1,31 @@
 describe('photoController', function() {
-      beforeEach(function() {
+  beforeEach(function() {
+    module('hotelligence');
+  });
 
-        module('hotelligence');
+  describe('photoController', function() {
+    var ctrl;
+
+    beforeEach(inject(function($controller) {
+      ctrl = $controller('photoController');
+    }));
+
+    it('initialises without a user', function() {
+      expect(ctrl.user).toBeUndefined();
+    });
+
+    describe('#takephoto', function() {
+
+      it('should tell the webcam to freeze', function(){
+        spyOn(Webcam, "freeze");
+        ctrl.takePhoto();
+        expect(Webcam.freeze).toHaveBeenCalled();
       });
 
-      describe('photoController', function() {
-
-        var ctrl;
-
-        beforeEach(inject(function($controller) {
-          ctrl = $controller('photoController');
-        }));
-
-        it('initialises without a user', function() {
-          // ctrl = $controller('photoController');
-          expect(ctrl.user).toBeUndefined();
-
-        });
-
-        describe('#takephoto', function() {
-
-
-          it('should tell the webcam to freeze', function(){
-            spyOn(Webcam, "freeze");
-            ctrl.takePhoto();
-            expect(Webcam.freeze).toHaveBeenCalled();
-          });
-
-          it('should set more buttons to be true', function(){
-            ctrl.takePhoto();
-            expect(moreButtons).toBeTruthy();
-          });
-
-
-        });
+      it('should set more buttons to be true', function(){
+        ctrl.takePhoto();
+        expect(moreButtons).toBeTruthy();
+      });
     });
+  });
 });
