7927d698418110187e0cfe56cdd02ca63f654d58
Chris Wynne <chris.wynndow@gmail.com>
2016-02-11 17:38:09 +0000

    Fixes missing ref deleted by an idiot

--- app/components/pictureStore/storeFactory.js
+++ app/components/pictureStore/storeFactory.js
@@ -5,7 +5,8 @@ angular.module('myApp.pictureStore', ['firebase', 'firebase.utils', 'firebase.au
     return {
       saveInDb: function() {
         Webcam.snap(function(data) {
-          var uid = ref.getAuth().uid;
+          var db = new Firebase('https://hotel-check-in.firebaseio.com/');
+          var uid = db.getAuth().uid;
           var ref = new Firebase('https://hotel-check-in.firebaseio.com/users/' + uid);
           ref.update({
             image: data
