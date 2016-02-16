b543b3957ed6f1925ea98f5d9ff85ad2abf5db14
vegim qehaja <med.batt.uk@gmail.com>
2016-02-16 11:13:21 +0000

    adds data tablse folder

---
+++ app/dashTables/dataTable.js
@@ -0,0 +1,35 @@
+angular.module('hotelligence.dataTable', ['datatables', 'firebase', 'firebase.utils', 'firebase.auth', 'hotelligence.dataBaseFactory', 'ngRoute'])
+  .controller('DataTableCtrl', ['$firebaseArray', '$firebaseObject', 'fbutil', '$location', 'DatabaseFactory',
+
+    function DataTableCtrl($firebaseArray, $firebaseObject, DatabaseFactory) {
+      var self = this;
+      self.imageArray = [];
+      var db = new Firebase('https://hotel-check-in.firebaseio.com/');
+      var uid = db.getAuth().uid;
+      var bookingId = uid + '_hotelBookings;'
+      console.log('id outside avater method :' + uid);
+
+      $firebaseArray(db.child('users').child(uid).child('bookings')).$loaded().then(function(bookings) {
+        var hotelBookings = bookings;
+        console.log(hotelBookings);
+        self.bookings = hotelBookings;
+      });
+
+      self.getImageAvatar = function(uid) {
+        console.log('id inside :' + uid);
+        console.log('image avatar function');
+        var avatarFB = this;
+        var db = new Firebase('https://hotel-check-in.firebaseio.com/');
+        console.log('id inside :' + uid);
+
+        $firebaseObject(db.child('users').child(uid).child('image')).$loaded().then(function(image) {
+          console.log('image $value');
+          console.log(image.$value);
+          avatarFB.images = image.$value;
+          return image.$value;
+        });
+
+      };
+
+    }
+  ]);
