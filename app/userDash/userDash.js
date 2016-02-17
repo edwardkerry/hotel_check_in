0b9bf46184b50a502cb5bdd58ff488e909eadd52
Richard Moss <richard@englishtutorslive.com>
2016-02-17 16:33:16 +0000

    adds logic to userDash for docSubmit

--- app/userDash/userDash.js
+++ app/userDash/userDash.js
@@ -1,69 +1,84 @@
 (function(angular) {
   "use strict";
 
-  var app = angular.module('hotelligence.userDash', ['firebase.auth', 'firebase', 'firebase.utils', 'ngRoute']);
+  var app = angular.module('hotelligence.userDash', ['firebase.auth', 'firebase', 'firebase.utils', 'ngRoute', 'hotelligence.databaseFactory']);
 
-  app.controller('userDashController', ['$scope', 'fbutil', 'user', '$firebaseObject', 'FBURL', function ($scope, fbutil, user, $firebaseObject, FBURL) {
+  app.controller('userDashController', ['$scope', 'fbutil', 'user', '$firebaseObject', 'FBURL', 'DatabaseFactory', function($scope, fbutil, user, $firebaseObject, FBURL, DatabaseFactory) {
     $scope.syncedValue = $firebaseObject(fbutil.ref('syncedValue'));
     $scope.user = user;
     $scope.FBURL = FBURL;
     $scope.showModal = false;
     $scope.pic = null;
-    $scope.toggleModal = function(){
+    var db = new Firebase('https://hotel-check-in.firebaseio.com/');
+    $scope.uid = db.getAuth().uid;
+    $scope.docSubmit = false;
+
+    $scope.toggleModal = function() {
       $scope.showModal = !$scope.showModal;
+    };
+    $scope.getImageAvatar = function() {
+      var answerFinal;
+      console.log("inside getimage" + $scope.uid);
+      var photoAnswer = DatabaseFactory.getUserImage($scope.uid);
+      photoAnswer.then(function(answerFinal) {
+        $scope.images = answerFinal;
+      });
+       if(answerFinal !== ""){
+        $scope.docSubmit = true;
+      }
     };
   }]);
 
-  app.config(['$routeProvider', function ($routeProvider) {
+  app.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/userDash', {
       templateUrl: 'userDash/userDash.html',
       controller: 'userDashController',
       resolve: {
-        user: ['Auth', function (Auth) {
+        user: ['Auth', function(Auth) {
           return Auth.$waitForAuth();
         }]
       }
     });
   }]);
 
-  app.directive('modal', function(){
+  app.directive('modal', function() {
     return {
       template: '<div class="modal fade">' +
-          '<div class="modal-dialog">' +
-            '<div class="modal-content">' +
-              '<div class="modal-header">' +
-                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
-                '<h4 class="modal-title">{{ title }}</h4>' +
-              '</div>' +
-              '<div class="modal-body" ng-transclude></div>' +
-            '</div>' +
-          '</div>' +
+        '<div class="modal-dialog">' +
+        '<div class="modal-content">' +
+        '<div class="modal-header">' +
+        '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+        '<h4 class="modal-title">{{ title }}</h4>' +
+        '</div>' +
+        '<div class="modal-body" ng-transclude></div>' +
+        '</div>' +
+        '</div>' +
         '</div>',
-        restrict: 'E',
-        transclude: true,
-        replace: true,
-        scope: true,
-        link: function postLink(scope, element, attrs){
-          scope.title = attrs.title;
+      restrict: 'E',
+      transclude: true,
+      replace: true,
+      scope: true,
+      link: function postLink(scope, element, attrs) {
+        scope.title = attrs.title;
 
-          scope.$watch(attrs.visible, function(value) {
-            if(value === true)
+        scope.$watch(attrs.visible, function(value) {
+          if (value === true)
             $(element).modal('show');
-            else
+          else
             $(element).modal('hide');
-          });
+        });
 
-          $(element).on('shown.bs.modal', function(){
-            scope.$apply(function(){
-              scope.$parent[attrs.visible] = true;
-            });
+        $(element).on('shown.bs.modal', function() {
+          scope.$apply(function() {
+            scope.$parent[attrs.visible] = true;
           });
-          $(element).on('hidden.bs.modal', function(){
-            scope.$apply(function(){
-              scope.$parent[attrs.visible] = false;
-            });
+        });
+        $(element).on('hidden.bs.modal', function() {
+          scope.$apply(function() {
+            scope.$parent[attrs.visible] = false;
           });
-        }
+        });
+      }
     };
   });
 })(angular);
